3be683f151b6286bce1b5c99b058dfa69437f089
chavezvaladezjuan-ai <chavezvaladezjuan@gmail.com>
2025-11-26 16:05:25 -0600

    Update api.php

--- WebPrueba/routes/api.php
+++ WebPrueba/routes/api.php
@@ -8,11 +8,15 @@ use App\Http\Controllers\Api\AuthController;
 // Rutas públicas de autenticación
 Route::post('/register', [AuthController::class, 'register'])->name('api.register');
 Route::post('/login', [AuthController::class, 'login'])->name('api.login');
-Route::post('/logout', [AuthController::class, 'logout']);
 
-// Rutas de tareas con nombres
-Route::get('/tasks', [TaskController::class, 'index'])->name('api.tasks.index');
-Route::get('/tasks/{task}', [TaskController::class, 'show'])->name('api.tasks.show');
-Route::post('/tasks', [TaskController::class, 'store'])->name('api.tasks.store');
-Route::put('/tasks/{task}', [TaskController::class, 'update'])->name('api.tasks.update');
-Route::delete('/tasks/{task}', [TaskController::class, 'destroy'])->name('api.tasks.destroy');
+// SOLUCIÓN ALTERNATIVA: Middleware personalizado que acepta ambos
+Route::middleware(['web', 'auth:sanctum'])->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+
+    // Rutas de tareas
+    Route::get('/tasks', [TaskController::class, 'index'])->name('api.tasks.index');
+    Route::get('/tasks/{task}', [TaskController::class, 'show'])->name('api.tasks.show');
+    Route::post('/tasks', [TaskController::class, 'store'])->name('api.tasks.store');
+    Route::put('/tasks/{task}', [TaskController::class, 'update'])->name('api.tasks.update');
+    Route::delete('/tasks/{task}', [TaskController::class, 'destroy'])->name('api.tasks.destroy');
+});
